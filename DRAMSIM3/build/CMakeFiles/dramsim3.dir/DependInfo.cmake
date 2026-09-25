c92865caf7a337ca3b26d1a24b7d568928c8ff6d
Muhammad Junaid <muhammadjunaid@intensivate.com>
2026-09-25 13:14:11 -0400

    commited the dram build, so new users don't have to build it themselves

---
+++ DRAMSIM3/build/CMakeFiles/dramsim3.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/DRAMSIM3/src/bankstate.cc" "CMakeFiles/dramsim3.dir/src/bankstate.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/bankstate.cc.o.d"
+  "/workspace/DRAMSIM3/src/channel_state.cc" "CMakeFiles/dramsim3.dir/src/channel_state.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/channel_state.cc.o.d"
+  "/workspace/DRAMSIM3/src/command_queue.cc" "CMakeFiles/dramsim3.dir/src/command_queue.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/command_queue.cc.o.d"
+  "/workspace/DRAMSIM3/src/common.cc" "CMakeFiles/dramsim3.dir/src/common.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/common.cc.o.d"
+  "/workspace/DRAMSIM3/src/configuration.cc" "CMakeFiles/dramsim3.dir/src/configuration.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/configuration.cc.o.d"
+  "/workspace/DRAMSIM3/src/controller.cc" "CMakeFiles/dramsim3.dir/src/controller.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/controller.cc.o.d"
+  "/workspace/DRAMSIM3/src/cosimulation.cc" "CMakeFiles/dramsim3.dir/src/cosimulation.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/cosimulation.cc.o.d"
+  "/workspace/DRAMSIM3/src/dram_system.cc" "CMakeFiles/dramsim3.dir/src/dram_system.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/dram_system.cc.o.d"
+  "/workspace/DRAMSIM3/src/hmc.cc" "CMakeFiles/dramsim3.dir/src/hmc.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/hmc.cc.o.d"
+  "/workspace/DRAMSIM3/src/memory_system.cc" "CMakeFiles/dramsim3.dir/src/memory_system.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/memory_system.cc.o.d"
+  "/workspace/DRAMSIM3/src/refresh.cc" "CMakeFiles/dramsim3.dir/src/refresh.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/refresh.cc.o.d"
+  "/workspace/DRAMSIM3/src/simple_stats.cc" "CMakeFiles/dramsim3.dir/src/simple_stats.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/simple_stats.cc.o.d"
+  "/workspace/DRAMSIM3/src/timing.cc" "CMakeFiles/dramsim3.dir/src/timing.cc.o" "gcc" "CMakeFiles/dramsim3.dir/src/timing.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DRAMSIM3/build/CMakeFiles/dramsim3main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/DRAMSIM3/src/cpu.cc" "CMakeFiles/dramsim3main.dir/src/cpu.cc.o" "gcc" "CMakeFiles/dramsim3main.dir/src/cpu.cc.o.d"
+  "/workspace/DRAMSIM3/src/main.cc" "CMakeFiles/dramsim3main.dir/src/main.cc.o" "gcc" "CMakeFiles/dramsim3main.dir/src/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspace/DRAMSIM3/build/CMakeFiles/dramsim3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
